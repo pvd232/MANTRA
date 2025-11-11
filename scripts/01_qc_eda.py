5c1de4cf0a07e606d345e2b83de6bb43296743b9
pvd232 <50126818+pvd232@users.noreply.github.com>
2025-11-11 09:20:11 -0500

    update qc read

--- scripts/01_qc_eda.py
+++ scripts/01_qc_eda.py
@@ -31,7 +31,7 @@ def main() -> None:
     out_dir.mkdir(parents=True, exist_ok=True)
 
     # Load AnnData
-    ad = sc.read_h5ad(args.adata, backed="r").copy()
+    ad = sc.read_h5ad(args.adata).copy()
     print("ad", ad)
     print("ad.obs", ad.obs)
     print()
